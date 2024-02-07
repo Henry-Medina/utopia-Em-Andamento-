50209efbb39c30220fcccc5245462bf6b1e18a36
Henry <147109974+Henry-Medina@users.noreply.github.com>
2024-02-07 15:55:09 -0300

    Add files via upload

---
+++ UTOPIA/src/js/index.js
@@ -0,0 +1,21 @@
+const botao = document.querySelector(".btn-plataforma");
+const elementoPlataformas = document.querySelector(".btn-plataforma .plataformas");
+
+botao.addEventListener("click", () => {
+    elementoPlataformas.classList.toggle("ativo");
+});
+
+let contador = 1;
+
+setInterval( function(){
+    document.getElementById('slide' + contador).checked = true;
+    contador++;
+
+    if(counter > 5 ) {
+        contador = 1;
+    }
+}, 3000 );
+
+function voltar() {
+    window.history.back();
+}
